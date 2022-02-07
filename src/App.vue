ad7327fc4725f16f4139ffa7ff30688b480b40cc
Sebastian Ahumada <cbasmx@gmail.com>
2022-02-07 16:59:57 -0300

    link

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   
   <router-view/>
+
+  <div class="text-center mt-5 mb-3">
+  <img src="@/assets/logo.png" class="rounded" style="max-width:30px;"  alt="..."> <a href="https://vuejs.org/" target="_blank">Vue.js</a>
+</div>
 </template>
 
 <style>
@@ -19,6 +23,16 @@
 #nav a {
   font-weight: bold;
   color: #2c3e50;
+}
+
+a{
+  font-weight: bold;
+  color: #2c3e50;
+  text-decoration: none;
+}
+
+a:hover{
+  color:#42b983
 }
 
 #nav a.router-link-exact-active {
